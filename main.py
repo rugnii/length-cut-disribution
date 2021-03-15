4f357222782712dc2d9a696e8d4aba574eee1d82
Ingur Boettger <mail@ingurboettger.com>
2021-03-15 14:48:59 +0100

    reading data in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-import numpy as numpy
+import numpy as np
 class supply:
     def __init__(self, length):
         self.setLength(length)
@@ -23,4 +23,24 @@ class main:
         s1.cut(cut1)
         print(s1.getRest())
 
-m = main(500)
+
+with open('values.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    cuts = np.empty(1)
+    cuts = []
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            #cuts = np.append(cuts, row[1])
+            cuts.append(int(row[1]))
+            #print(f'\t{row[0]} works in the {row[1]} department, and was born in.')
+            #line_count += 1
+    print(f'Processed {line_count} lines.')
+    npCuts = np.array(cuts)
+    npCuts = np.flip(np.sort(npCuts))
+    cutsUsed = np.array([npCuts, np.zeros(len(npCuts))])
+    cutsUsed = cutsUsed.reshape(len(npCuts),2, order='F')
+    print(cutsUsed)
